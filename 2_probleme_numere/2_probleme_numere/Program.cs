e64673800f95aba5dde7a20e7ecdbcae1b92934f
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 11:30:08 +0200

    Problema 2

---
+++ 2_probleme_numere/2_probleme_numere/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_probleme_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, x1, x2, d;
+            Console.WriteLine("Dati coeficientii ecuatiei");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = float.Parse(Console.ReadLine());
+            Console.WriteLine("{0}x^2+{1}x+{2}=0", a, b, c);
+            if (a != 0)
+            {
+                d = b * b - 4 * a * c;
+                if (d < 0)
+                    Console.WriteLine("Ecuatia nu are radacini reale") ;
+                else
+                    if(d==0)
+                    {
+                        x1 = (float)(-b + Math.Sqrt(d)) / (2 * a);
+                        Console.WriteLine("x1=x2={0}", x1);
+                    }
+                else
+                {
+                    x1 = (float)(-b + Math.Sqrt(d)) / (2 * a);
+                    x2 = (float)(-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine("X1={0}", x1);
+                    Console.WriteLine("X2={0}", x2);
+                }
+            }
+            else
+            {
+                if (b != 0)
+                {
+                    x1 = -c / b;
+                    Console.WriteLine("Ec grad 1: X1={0}", x1);
+                }
+                else
+                {
+                    if (c == 0)
+                        Console.WriteLine("Identitate");
+                    else
+                        Console.WriteLine("Ecuatie imposibila");
+                }
+            }
+
+        }
+    }
+}
