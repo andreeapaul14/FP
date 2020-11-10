3ac14bd8c5a4ae4c230ecb297ce23e01a3fe5cf0
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 15:42:22 +0200

    Problema 8

---
+++ 8_probleme_cu_numere/8_probleme_cu_numere/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Introduceti numarul a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul b: ");
+            b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("Numarul a={0} si b={1} ", a, b);
+        }
+    }
+}
