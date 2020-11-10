aeeb07ab8f710466a2cd585164bdb46cfcbf2ba7
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 11:19:49 +0200

    Problema 1

---
+++ 1_probleme_numere/1_probleme_numere/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_probleme_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, x;
+            Console.WriteLine("Dati coeficientii ecuatiei");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            if (a != 0)
+            {
+                x = -b / a;
+                Console.WriteLine("Solutia = {0}", x);
+            }
+            else
+                 Console.WriteLine("Ecuatia nu este de gradul I");
+        }
+    }
+}
