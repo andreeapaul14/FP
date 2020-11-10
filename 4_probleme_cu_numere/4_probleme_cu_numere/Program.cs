f1c81cd5134025e36242b611e56af388ae81c8f8
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 15:15:15 +0200

    Problema 4

---
+++ 4_probleme_cu_numere/4_probleme_cu_numere/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int an;
+            an = int.Parse(Console.ReadLine());
+            if ((an % 4 == 0 && an%100!=0) || an%400==0)
+                Console.WriteLine("Anul {0} este an bisect", an);
+            else
+                Console.WriteLine("Anul {0} nu este an bisect", an);
+        }
+    }
+}
