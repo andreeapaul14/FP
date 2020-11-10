f9ba4deadf4a804e45bc5bee2180d40e06884654
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 16:02:43 +0200

    Problema 9

---
+++ 9_probleme_cu_numere/9_probleme_cu_numere/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Divizorii lui {0} sunt: ",n);
+            for (int d = 1; d <= n; d++)
+                if (n % d == 0)
+                {
+                    Console.Write(d);
+                    Console.Write(" ");
+                }
+        }
+    }
+}
