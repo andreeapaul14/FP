993d02341c23d836329fbe50fc96633c940aa392
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 15:22:56 +0200

    Problema 5

---
+++ 5_probleme_cu_numere/5_probleme_cu_numere/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n;
+            Console.WriteLine("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul k: ");
+            k = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= k-1; i++) 
+                n /= 10;
+            Console.WriteLine("A k-a cifra din numar este {0}", n % 10);
+        }
+    }
+}
