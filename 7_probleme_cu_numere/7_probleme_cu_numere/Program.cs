a1aeed42dad2e2c37ff515bc571eb6500f8823e9
Andreea <andreeapaul14@yahoo.ro>
2020-11-10 15:36:12 +0200

    Problema 7

---
+++ 7_probleme_cu_numere/7_probleme_cu_numere/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,aux;
+            Console.WriteLine("Introduceti numarul a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul b: ");
+            b = int.Parse(Console.ReadLine());
+            aux = a;
+            a = b;
+            b = aux;
+            Console.WriteLine("Numarul a={0} si b={1} ",a,b);
+        }
+    }
+}
