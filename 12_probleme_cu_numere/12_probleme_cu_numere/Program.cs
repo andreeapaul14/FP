60647e09bb694a1467285f29c7a0fcfba2d01a02
Andreea <andreeapaul14@yahoo.ro>
2020-11-11 12:21:50 +0200

    Problema 12

---
+++ 12_probleme_cu_numere/12_probleme_cu_numere/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, n,c=0;
+            Console.WriteLine("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul b: ");
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+                if (i % n == 0)
+                    c++;
+            Console.WriteLine($"In intervalul [{a},{b}] sunt {c} numere intregi divizibile cu {n}");
+        }
+    }
+}
