5d136b9ee2b1eea1493c7da10bfc55441d7456a8
Andreea <andreeapaul14@yahoo.ro>
2020-11-11 12:40:01 +0200

    Problema 14

---
+++ 14_probleme_cu_numere/14_probleme_cu_numere/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, p=0;
+            Console.WriteLine("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            c = n; //c este copia lui n
+            while(c!=0)
+            {
+                p = p * 10 + c % 10;
+                c /= 10;
+            }
+            if (p == n)
+                Console.WriteLine($"Numarul {n} este palindrom");
+            else
+                Console.WriteLine($"Numarul {n} nu este palindrom");
+        }
+    }
+}
