510a4a1a363b33369e880991fb17b7666197406c
Andreea <andreeapaul14@yahoo.ro>
2020-11-11 16:44:26 +0200

    Problema 17

---
+++ 16_probleme_cu_numere/16_probleme_cu_numere/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, min, max;
+            Console.WriteLine("Introduceti 5 numere: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            if (a < b)
+            {
+                if (a < c && a < d && a < e)
+                {
+                    Console.Write($"{a} ");
+                    if (b < c)
+                    {
+                        if (b < d && b < e)
+                        {
+                            Console.Write($"{b} ");
+                            if (c < d)
+                            {
+                                if (c < e)
+                                {
+                                    Console.Write($"{c} ");
+                                    if (d < e)
+                                        Console.WriteLine($"{d} {e}");
+                                    else
+                                        Console.WriteLine($"{e} {d}");
+                                }
+                                else
+                                {
+                                    if(e<c)
+                                    {
+                                        Console.Write($"{e} ");
+                                        if(e<d)
+                                            Console.WriteLine($"{c} {d}");
+                                        else
+                                            Console.WriteLine($"{d} {c}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ 17_probleme_cu_numere/17_probleme_cu_numere/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_probleme_cu_numere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,x,y,z=0;
+            Console.WriteLine("Introduceti 2 numere intregi: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            z = x * y / a;
+            Console.WriteLine($"Cel mai mare divizor al numerelor {x} si {y} este {a}");
+            Console.WriteLine($"Cel mai mic multiplu comun al numerelor {x} si {y} este {z}");
+        }
+    }
+}
