27f3a30bd9739602e086cd152726003174518e55
Andreea <andreeapaul14@yahoo.ro>
2020-12-16 12:15:22 +0200

    pool2- problema 8

---
+++ 8_probleme_cu_secvente/8_probleme_cu_secvente/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_probleme_cu_secvente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul n");
+            int n = int.Parse(Console.ReadLine());
+            int firstNumber = 1, secondNumber = 1;
+            int fn;
+
+
+            for (int i = 2; i < n; i++)
+            {
+                fn= firstNumber + secondNumber;
+                secondNumber = firstNumber;
+                firstNumber = fn;
+            }
+            Console.WriteLine ("Al {0}-lea termen din sirul fibonacci este {1} ", n, firstNumber);
+        }
+    }
+}
