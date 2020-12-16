6d73bd706f13d487081081a9aafe302ffc9ff0b3
Andreea <andreeapaul14@yahoo.ro>
2020-12-16 11:07:52 +0200

    pool2 - problema 3

---
+++ 3_probleme_cu_secvente/3_probleme_cu_secvente/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_probleme_cu_secvente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, s = 0,p=1;
+            Console.WriteLine("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                s = s + i;
+                p = p * i;
+            }
+            Console.WriteLine("Suma numerelor de la 1 la {0} este {1}", n, s);
+            Console.WriteLine("Produsul numerelor de la 1 la {0} este {1}", n, p);
+        }
+    }
+}
