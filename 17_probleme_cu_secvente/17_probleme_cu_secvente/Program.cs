02e88181ce3651bd51a8e5ea19f479672afe633a
Andreea <andreeapaul14@yahoo.ro>
2021-01-04 16:24:02 +0200

    problema1_pool 3

---
+++ 17_probleme_cu_secvente/17_probleme_cu_secvente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_probleme_cu_secvente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(x=Read())
+            { }
+        }
+    }
+}
---
+++ 1_pool3/1_pool3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_pool3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, s = 0;
+            int[] a = new int[100];
+            Console.WriteLine("Dati nr de elemente de initializat din vector");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n ; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                s = s + a[i];
+            }
+            Console.WriteLine("Suma={0}", s);
+        }
+    }
+}
