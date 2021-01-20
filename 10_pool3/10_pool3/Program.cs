a91a86b2aebb645c44a92bf6d0869f4efa9cbb64
Andreea <andreeapaul14@yahoo.ro>
2021-01-20 21:52:48 +0200

    Problema 10_pool3

---
+++ 10_pool3/10_pool3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_pool3
+{
+    class Program
+    {/// <summary>
+     /// Cautare binara. 
+     /// Se da un vector cu n elemente sortat in ordine crescatoare.
+     /// Se cere sa se determine pozitia unui element dat k.
+     /// Daca elementul nu se gaseste in vector rezultatul va fi -1.
+     /// </summary>
+     /// <param name="args"></param>
+     /// 
+        static int n, x, m;
+        static int[] v;
+        static void Main(string[] args)
+        {
+            n = 0;
+            x = 0;
+            m = 0;
+
+            Console.Write("n= ");
+            n = int.Parse(Console.ReadLine());
+
+            v = new int[n];
+
+            Console.Write("x= ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti elemente in ordine crescatoare: ");
+
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Elementul a fost gasit pe pozitia {cautare(0, n - 1)}!");
+            Console.ReadKey();
+        }
+
+        static int cautare(int s, int d)
+        {
+            if (s > d)
+                return -1;
+
+            m = (s + d) / 2;
+            if (x == v[m])
+                return m;
+            if (x < v[m])
+                return cautare(s, m - 1);
+            else
+                return cautare(m + 1, d);
+        }
+    }
+}
