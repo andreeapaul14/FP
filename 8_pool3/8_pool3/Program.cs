49af1b71d865015974ba3a21d9b15962ff1f5768
Andreea <andreeapaul14@yahoo.ro>
2021-01-20 21:31:53 +0200

    Problema 8-pool3

---
+++ 8_pool3/8_pool3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_pool3
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("introduceti numarul de elemente: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = rnd.Next(10);
+                v[i] = x;
+                Console.Write($"{v[i]} ");
+            }
+            int y = v[0]; //memorarea primului element
+            for (int i = 1; i < n; i++)
+            {
+                v[i - 1] = v[i]; //rotirea spre stanga
+            }
+            v[n - 1] = y; //actualizarea ultimului element cu primul
+
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+                Console.Write($"{v[i]} ");
+        }
+    }
+}
